92f1142526b7b0279c77ecfb88387a9bf0443356
Rai <rai.conslog@hotmail.com>
2025-12-22 01:47:42 -0300

    chore: initial Spring Boot setup with PostgreSQL and dev profile

---
+++ src/main/java/com/limochat/LimoChatApplication.java
@@ -0,0 +1,14 @@
+package com.limochat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimoChatApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(LimoChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/limochat/LimoChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.limochat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimoChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
